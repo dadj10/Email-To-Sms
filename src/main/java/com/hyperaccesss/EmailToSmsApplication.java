441b617a7032c31c94fb51610d6bccd46aced1c5
Désiré Adjé <adjedesire1995@gmail.com>
2020-10-21 14:25:54 +0000

    first commit

---
+++ src/main/java/com/hyperaccesss/EmailToSmsApplication.java
@@ -0,0 +1,13 @@
+package com.hyperaccesss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailToSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailToSmsApplication.class, args);
+	}
+
+}
